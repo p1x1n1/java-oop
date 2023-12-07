f9c851a4e3b1a3f6fabb6a5869ab1dd1e61bdce5
bazina anastasia <abazina858@gmail.com>
2023-12-07 13:48:10 +0400

    5.2 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,76 @@
 import java.io.*;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
+            LinkedList<undead> undeads = new LinkedList<>();
+            LinkedList<vampire> vampires = new LinkedList<>();
+            LinkedList<wizard> wizards = new LinkedList<>();
+            boolean b = true;
+            while (b){
+                System.out.println("Выберите действие: \n" +
+                        "1.Создать персонажа \n"+
+                        "2.Посмотреть клан \n"+
+                        "3.Тест способностей");
+                Scanner sc = new Scanner(System.in);
+                int i = sc.nextInt();
+                switch (i){
+                    case 1:
+                        System.out.println("Выберите действие: \n" +
+                                "1.Создать вампира \n"+
+                                "2.Создать волшебника");
+                        i= sc.nextInt();
+                        System.out.println("Введите имя:");
+                        String name= sc.nextLine();
+                        name= sc.nextLine();
+                        System.out.println("Введите уровень:");
+                        int level = sc.nextInt();
+                        System.out.println("Введите количество способности:");
+                        int abcnt= sc.nextInt();
+                        switch (i) {
+                            case 1:
+                                vampires.add(new vampire(name,level,abcnt));
+                                undeads.add(vampires.peekLast());
+                                break;
+                            case 2:
+                                wizards.add(new wizard(name,level,abcnt));
+                                undeads.add(wizards.peekLast());
+                                break;
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Выберите действие: \n" +
+                                "1.Посмотреть клан вампиров \n"+
+                                "2.Посмотреть клан волшебников \n" +
+                                "3.Посмотреть клан нежити");
+                        i= sc.nextInt();
+                        switch (i) {
+                            case 1:
+                                System.out.println(vampires);
+                                break;
+                            case 2:
+                                System.out.println(wizards);
+                                break;
+                            case 3:
+                                System.out.println(undeads);
+                                break;
+                        }
+                        break;
+                    case 3:
+                        for (int j = 0,power; j< undeads.size() ; j++) {
+                            System.out.println(undeads.get(j));
+                            power = undeads.get(j).use_abilities();
+                            System.out.println("с силой: "+power);
+                            }
+                        break;
+                }
+                System.out.println("Введите true чтобы продолжить, иначе введите false");
+                b= sc.nextBoolean();
+            }
+            /*
             vampire V1 = new vampire("Kaleb", 2, 40);
             vampire V2 = new vampire("Lilit", 2, 36);
             vampire V3 = new vampire("Vladislavus", 2, 56);
@@ -46,23 +112,34 @@ public class Main {
             //equals
             //System.out.println(W1.equals(V1) + " " + W1.equals(W2) + " " + W1.equals(W1) + " " + W1.equals(W4));
             //System.out.println(V1.equals(V3) + " " + V1.equals(V1) + " " + V4.equals(V3));
-            //5.2
-            //byte[] buffer = V1.toString().getBytes();//преобразуем строку к массиву байт
+            */
+            //5.2.1
+            vampire V1 = new vampire("Kaleb", 2, 40);
+            wizard W1 = new wizard("Morgan", 3, 33);
+
+            //out
             FileOutputStream fos = new FileOutputStream("C:\\Users\\1\\OneDrive\\Документы\\вуз\\3 курс\\java\\lab4-OOP-sims\\outputstream.txt");//Класс FileOutputStream создаёт объект класса OutputStream, который можно использовать для записи байтов в файл.
+            UndeadOutput.output_undead(W1,fos);
+            //W1.output(fos);
+
+            //write
             File file = new File("C:\\Users\\1\\OneDrive\\Документы\\вуз\\3 курс\\java\\lab4-OOP-sims\\outputwriter.txt");
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            V1.write(fw);
-            V1.output(fos);
+            UndeadOutput.writeundead(V1,fw);
+            //V1.write(fw);
 
+            //read
             Reader reader = new FileReader(file);
             System.out.println();
-            undead v=UndeadOutput.readundead(reader);
+            undead v= UndeadOutput.readundead(reader);
             System.out.println(v);
 
+            //input
             DataInputStream dis = new DataInputStream(new FileInputStream("C:\\Users\\1\\OneDrive\\Документы\\вуз\\3 курс\\java\\lab4-OOP-sims\\outputstream.txt"));
             v = UndeadOutput.inputundead(dis);
             System.out.println(v);
 
+            //5.2.2
             FileOutputStream outputStream = new FileOutputStream("C:\\Users\\1\\OneDrive\\Документы\\вуз\\3 курс\\java\\lab4-OOP-sims\\save.ser");
             FileInputStream inputStream = new FileInputStream("C:\\Users\\1\\OneDrive\\Документы\\вуз\\3 курс\\java\\lab4-OOP-sims\\save.ser");
             UndeadOutput.serializeUndead(V1,outputStream);
